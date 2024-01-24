a2b91a5a4e50f283bf567c5a7f49ad49ce288f11
xakqngel <arkqngel343@gmail.com>
2024-01-24 12:47:16 -0500

    stack done, thanks to monoald

---
+++ src/const.ts
@@ -0,0 +1,66 @@
+export const stack = [
+    {
+      name: "HTML",
+      color: "#EF652A",
+    },
+    {
+      name: "CSS",
+      color: "#30A9DC",
+    },
+    {
+      name: "JavaScript",
+      color: "#F0DB4F",
+    },
+    {
+      name: "TypeScript",
+      color: "#3178C6",
+    },
+    {
+      name: "Tailwind",
+      color: "#17BAB8",
+    },
+    {
+      name: "React",
+      color: "#00D8FF",
+    },
+    {
+      name: "Zustand",
+      color: "#764ABC",
+    },
+    {
+      name: "Next",
+      color: "#FAFAFA",
+    },
+    {
+      name: "Astro",
+      color: "#FAFAFA",
+    },
+    {
+      name: "NodeJS",
+      color: "#539E43",
+    },
+    {
+      name: "ExpressJS",
+      color: "#FAFAFA",
+    },
+    {
+      name: "Jest",
+      color: "#99425B",
+    },
+    {
+      name: "Git",
+      color: "#DE4C36",
+    },
+    {
+      name: "Linux",
+      color: "#DFAF22",
+    },
+    {
+      name: "VS Code",
+      color: "#3DA9F2",
+    },
+    {
+      name: "Figma",
+      color: "#A95FFF",
+    },
+  ];
