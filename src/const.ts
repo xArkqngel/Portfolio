07d6a313b3bed7c57556c96d43120432f12f9823
xArkqngel <arkqngel343@gmail.com>
2024-07-10 10:44:19 -0500

    Portfolio mods, added CV, and more stack tech

--- src/const.ts
+++ src/const.ts
@@ -1,66 +1,122 @@
 export const stack = [
-    {
-      name: "HTML",
-      color: "#EF652A",
-    },
-    {
-      name: "CSS",
-      color: "#30A9DC",
-    },
-    {
-      name: "JavaScript",
-      color: "#F0DB4F",
-    },
-    {
-      name: "TypeScript",
-      color: "#3178C6",
-    },
-    {
-      name: "Tailwind",
-      color: "#17BAB8",
-    },
-    {
-      name: "React",
-      color: "#00D8FF",
-    },
-    {
-      name: "Zustand",
-      color: "#764ABC",
-    },
-    {
-      name: "Next",
-      color: "#FAFAFA",
-    },
-    {
-      name: "Astro",
-      color: "#FAFAFA",
-    },
-    {
-      name: "NodeJS",
-      color: "#539E43",
-    },
-    {
-      name: "ExpressJS",
-      color: "#FAFAFA",
-    },
-    {
-      name: "Jest",
-      color: "#99425B",
-    },
-    {
-      name: "Git",
-      color: "#DE4C36",
-    },
-    {
-      name: "Linux",
-      color: "#DFAF22",
-    },
-    {
-      name: "VS Code",
-      color: "#3DA9F2",
-    },
-    {
-      name: "Figma",
-      color: "#A95FFF",
-    },
-  ];
+  {
+    name: "JavaScript",
+    color: "#F0DB4F",
+  },
+  {
+    name: "TypeScript",
+    color: "#3178C6",
+  },
+  {
+    name: "React",
+    color: "#00D8FF",
+  },
+  {
+    name: "NextJS",
+    color: "#FAFAFA",
+  },
+  {
+    name: "Zustand",
+    color: "#764ABC",
+  }, 
+  {
+    name: "Astro",
+    color: "#FAFAFA",
+  },
+  {
+    name: "HTML",
+    color: "#EF652A",
+  },
+  {
+    name: "CSS",
+    color: "#30A9DC",
+  },
+  {
+    name: "Tailwind",
+    color: "#17BAB8",
+  },
+  {
+    name: "Bootstrap",
+    color: "#7952B3",
+  },
+  {
+    name: "MUI",
+    color: "#007FFF",
+  },
+  {
+    name: "SASS",
+    color: "#CC6699",
+  },
+  {
+    name: "Vite",
+    color: "#646CFF",
+  },
+  {
+    name: "Vitest",
+    color: "#646CFF",
+  },
+  {
+    name: "Apollo GraphQL",
+    color: "#311C87",
+  },
+  {
+    name: "Java",
+    color: "#007396",
+  },
+  {
+    name: "NodeJS",
+    color: "#539E43",
+  },
+  {
+    name: "Express",
+    color: "#FAFAFA",
+  },
+  {
+    name: "NestJS",
+    color: "#E0234E",
+  },
+  {
+    name: "Jest",
+    color: "#99425B",
+  },
+  {
+    name: "Cypress",
+    color: "#00C389",
+  },
+  {
+    name: "GraphQL",
+    color: "#E10098",
+  },
+  {
+    name: "MongoDB",
+    color: "#47A248",
+  },
+  {
+    name: "Postgres",
+    color: "#336791",
+  },
+  {
+    name: "MySQL",
+    color: "#4479A1",
+  },
+  {
+    name: "AWS",
+    color: "#FF9900",
+  },
+  {
+    name: "Supabase",
+    color: "#3ECF8E",
+  },
+  {
+    name: "Docker",
+    color: "#2496ED",
+  },
+  {
+    name: "Git",
+    color: "#DE4C36",
+  },
+  {
+    name: "Figma",
+    color: "#A95FFF",
+  },
+];
